efe7e2500d008eb28031bec499aa36cf7e88f826
coderzhao <coderzhaoziwei@outlook.com>
2021-07-14 22:00:07 +0800

    feat(module): update emitter module

--- src/store/modules/emitter.js
+++ src/store/modules/emitter.js
@@ -1,10 +1,9 @@
 export default {
-  state() {
-    return {
-      id: 0,
-      types: {},
-      handlers: {},
-    }
+  namespaced: true,
+  state: {
+    id: 0,
+    types: {},    // type: [ handlerId_1, handlerId_2, handlerId_3, ... ]
+    handlers: {}, // handlerId: { type, typeIndex, handler, handlerId }
   },
   mutations: {
     addHandler(state, { type, handler }) {
@@ -23,22 +22,26 @@ export default {
     },
   },
   actions: {
-    emit({ state, commit }, { type, data }) {
-      const ids = state.types[type]
-      if (Array.isArray(ids) === false) return
-      ids.forEach(id => {
-        if (id === undefined) return
-        const { handler, handlerId } = state.handlers[id]
-        if (typeof handler === "function") {
-          // 第二个参数为注销自身的方法
-          handler(data, () => commit("deleteHandler", handlerId))
-        }
-      })
+    emit: {
+      root: true,
+      handler({ state, commit }, { type, data }) {
+        const ids = state.types[type]
+        if (Array.isArray(ids) === false) return
+        ids.forEach(id => {
+          if (id === undefined) return
+          const { handler, handlerId } = state.handlers[id]
+          if (typeof handler === "function") {
+            handler(data, () => commit("deleteHandler", handlerId)) // 第二个参数为注销自身的方法
+          }
+        })
+      },
     },
-    on({ state, commit }, { type, handler }) {
-      commit("addHandler", { type, handler })
-      // 返回一个注销自身的方法
-      return () => commit("deleteHandler", state.id)
+    on: {
+      root: true,
+      handler({ state, commit }, { type, handler }) {
+        commit("addHandler", { type, handler })
+        return () => commit("deleteHandler", state.id) // 返回一个注销自身的方法
+      },
     },
   },
 }
